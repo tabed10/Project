037a8eb8b3c12e280d168a5ce1b4984f4b8070b6
tabed10 <sabbirhossainabed@gmail.com>
2019-07-18 01:54:42 +0600

    Update stats added(incomplete)

--- script.js
+++ script.js
@@ -179,3 +179,22 @@ function myFunction5(){
     cell18.innerHTML = competitions[1].players[i].stats[0].totalPoints
   }
 }
+
+function myFunction6(){
+  var $button = $(this);
+  var oldValue = $button.parent().find("input").val();
+
+  if ($button.text() == "+") {
+    var newVal = parseFloat(oldValue) + 1;
+	} else {
+   // Don't allow decrementing below zero
+    if (oldValue > 0) {
+      var newVal = parseFloat(oldValue) - 1;
+    } else {
+      newVal = 0;
+    }
+  }
+
+  $button.parent().find("input").val(newVal);
+  console.log("working")
+}
